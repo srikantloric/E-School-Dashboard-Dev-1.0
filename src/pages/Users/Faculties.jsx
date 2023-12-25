81be933cd4ce60c6e11d05092e76cbc9977b9f7c
kundan gupta <kundangupta1905@gmail.com>
2023-12-25 23:08:33 +0530

    faculty view create table facultyslice

--- src/pages/Users/Faculties.jsx
+++ src/pages/Users/Faculties.jsx
@@ -3,7 +3,10 @@ import Navbar from "../../components/Navbar/Navbar";
 import LSPage from "../../components/Utils/LSPage";
 import PageContainer from "../../components/Utils/PageContainer";
 import Card from "../../components/Card/Card";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 function Faculties() {
+ 
   return (
     <>
       <PageContainer>
